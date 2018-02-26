85b3b5cc9452770e865175bba34551413f59e8a4
Xiaotai Chai <xiaotaichai@Xiaotais-MBP.frontierlocal.net>
2018-02-25 22:57:13 -0500

    q2 done

--- Q2_the_count.py
+++ Q2_the_count.py
@@ -1,24 +1,43 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 import re
+
+WORD_RE = re.compile(r"[\w']+")
 
 class MRWordFrequencyCount(MRJob):
 
     def mapper(self, _, line):
-    	words = line.split()
-    	yield ("", words.lower())
-
-    def combiner(self, key, values):
-    	word = input("the")
-    	k = 0
-    	for i in values:
-    		if(i==word):
-    			k=k+1
-    	yield ("the", k)
+    	for word in WORD_RE.findall(line):
+    		yield ("", word.lower())
 
     def reducer(self, key, values):
-    	yield key, sum(k)
+    	k = 0
+    	for i in values:
+    		if(i == "the" or i == "The"):
+    			k=k+1
+    	yield ("the", k)
 
 
 if __name__ == '__main__':
     MRWordFrequencyCount.run()
+
+
+# from mrjob.job import MRJob
+
+# class MRWordFrequencyCount(MRJob):
+
+#     def mapper(self, _, line):
+#     	words = line.split()
+#     	yield ("", words)
+
+#     def reducer(self, key, values):
+#     	#word = input("the")
+#     	k = 0
+#     	for i in values:
+#     		if(i=='the'):
+#     			k=k+1
+#     	yield (key, k)
+
+
+# if __name__ == '__main__':
+#     MRWordFrequencyCount.run()
+
